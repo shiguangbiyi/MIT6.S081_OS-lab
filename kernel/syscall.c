e88d1302182caab5ae6d06a18ee9a56ec9f5964b
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-06 23:01:11 +0800

    完整注释syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -7,34 +7,40 @@
 #include "syscall.h"
 #include "defs.h"
 
-// Fetch the uint64 at addr from the current process.
+// 从当前进程中获取地址 addr 处的 uint64 值。
 int
 fetchaddr(uint64 addr, uint64 *ip)
 {
   struct proc *p = myproc();
+  // 检查地址合法性
   if(addr >= p->sz || addr+sizeof(uint64) > p->sz)
     return -1;
+  // 从地址 addr 处复制 sizeof(uint64) 字节数据到 ip 指向的地址
   if(copyin(p->pagetable, (char *)ip, addr, sizeof(*ip)) != 0)
     return -1;
   return 0;
 }
 
-// Fetch the nul-terminated string at addr from the current process.
-// Returns length of string, not including nul, or -1 for error.
+// 从当前进程中获取地址 addr 处的以 '\0' 结尾的字符串。
+// 返回字符串的长度（不包括 '\0' 结尾字符），如果出错返回 -1。
 int
 fetchstr(uint64 addr, char *buf, int max)
 {
   struct proc *p = myproc();
+  // 调用 copyinstr 函数复制以 '\0' 结尾的字符串到 buf 中，最多复制 max 个字符
   int err = copyinstr(p->pagetable, buf, addr, max);
   if(err < 0)
     return err;
+  // 返回字符串的长度（包括 '\0' 结尾字符）
   return strlen(buf);
 }
 
+// 获取第 n 个系统调用参数的原始值。
 static uint64
 argraw(int n)
 {
   struct proc *p = myproc();
+  // 根据 n 的值返回对应的系统调用参数
   switch (n) {
   case 0:
     return p->trapframe->a0;
@@ -49,11 +55,11 @@ argraw(int n)
   case 5:
     return p->trapframe->a5;
   }
-  panic("argraw");
+  panic("argraw"); // 如果 n 不在有效范围内，触发内核崩溃
   return -1;
 }
 
-// Fetch the nth 32-bit system call argument.
+// 获取第 n 个系统调用参数，存入指针 ip 指向的地址。
 int
 argint(int n, int *ip)
 {
@@ -61,9 +67,8 @@ argint(int n, int *ip)
   return 0;
 }
 
-// Retrieve an argument as a pointer.
-// Doesn't check for legality, since
-// copyin/copyout will do that.
+// 获取第 n 个系统调用参数，存入指针 ip 指向的地址，不检查合法性，
+// copyin/copyout 将会处理合法性检查。
 int
 argaddr(int n, uint64 *ip)
 {
@@ -71,18 +76,19 @@ argaddr(int n, uint64 *ip)
   return 0;
 }
 
-// Fetch the nth word-sized system call argument as a null-terminated string.
-// Copies into buf, at most max.
-// Returns string length if OK (including nul), -1 if error.
+// 获取第 n 个系统调用参数，并将其解释为以 '\0' 结尾的字符串，复制到 buf 中，最多复制 max 个字符。
+// 如果成功，返回字符串长度（包括 '\0' 结尾字符），如果出错返回 -1。
 int
 argstr(int n, char *buf, int max)
 {
   uint64 addr;
   if(argaddr(n, &addr) < 0)
     return -1;
+  // 调用 fetchstr 函数获取字符串并复制到 buf 中
   return fetchstr(addr, buf, max);
 }
 
+// 声明系统调用的实现函数
 extern uint64 sys_chdir(void);
 extern uint64 sys_close(void);
 extern uint64 sys_dup(void);
@@ -111,50 +117,50 @@ extern uint64 sys_connect(void);
 extern uint64 sys_pgaccess(void);
 #endif
 
+// 定义系统调用数组，保存每个系统调用的实现函数。
 static uint64 (*syscalls[])(void) = {
-[SYS_fork]    sys_fork,
-[SYS_exit]    sys_exit,
-[SYS_wait]    sys_wait,
-[SYS_pipe]    sys_pipe,
-[SYS_read]    sys_read,
-[SYS_kill]    sys_kill,
-[SYS_exec]    sys_exec,
-[SYS_fstat]   sys_fstat,
-[SYS_chdir]   sys_chdir,
-[SYS_dup]     sys_dup,
-[SYS_getpid]  sys_getpid,
-[SYS_sbrk]    sys_sbrk,
-[SYS_sleep]   sys_sleep,
-[SYS_uptime]  sys_uptime,
-[SYS_open]    sys_open,
-[SYS_write]   sys_write,
-[SYS_mknod]   sys_mknod,
-[SYS_unlink]  sys_unlink,
-[SYS_link]    sys_link,
-[SYS_mkdir]   sys_mkdir,
-[SYS_close]   sys_close,
+  [SYS_fork]    sys_fork,
+  [SYS_exit]    sys_exit,
+  [SYS_wait]    sys_wait,
+  [SYS_pipe]    sys_pipe,
+  [SYS_read]    sys_read,
+  [SYS_kill]    sys_kill,
+  [SYS_exec]    sys_exec,
+  [SYS_fstat]   sys_fstat,
+  [SYS_chdir]   sys_chdir,
+  [SYS_dup]     sys_dup,
+  [SYS_getpid]  sys_getpid,
+  [SYS_sbrk]    sys_sbrk,
+  [SYS_sleep]   sys_sleep,
+  [SYS_uptime]  sys_uptime,
+  [SYS_open]    sys_open,
+  [SYS_write]   sys_write,
+  [SYS_mknod]   sys_mknod,
+  [SYS_unlink]  sys_unlink,
+  [SYS_link]    sys_link,
+  [SYS_mkdir]   sys_mkdir,
+  [SYS_close]   sys_close,
 #ifdef LAB_NET
-[SYS_connect] sys_connect,
+  [SYS_connect] sys_connect,
 #endif
 #ifdef LAB_PGTBL
-[SYS_pgaccess] sys_pgaccess,
+  [SYS_pgaccess] sys_pgaccess,
 #endif
 };
 
-
-
+// 系统调用处理函数
 void
 syscall(void)
 {
   int num;
   struct proc *p = myproc();
 
-  num = p->trapframe->a7;
+  num = p->trapframe->a7; // 获取系统调用号
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    p->trapframe->a0 = syscalls[num]();
+    p->trapframe->a0 = syscalls[num](); // 调用对应系统调用的实现函数，并将返回值存入 a0 中
   } else {
     printf("%d %s: unknown sys call %d\n",
-            p->pid, p->name, num);
-    p->trapframe->a0 = -1;
+            p->pid, p->name, num); // 如果系统调用号不在有效范围内或对应的系统调用函数未实现，则打印错误信息。
+    p->trapframe->a0 = -1; // 返回错误标志 -1。
   }
 }
