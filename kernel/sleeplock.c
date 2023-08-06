b2b48218fc36ce42f45a1b31e226fc712985d110
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-06 22:41:43 +0800

    完整注释sleeplock.c

--- kernel/sleeplock.c
+++ kernel/sleeplock.c
@@ -1,4 +1,4 @@
-// Sleeping locks
+// 睡眠锁
 
 #include "types.h"
 #include "riscv.h"
@@ -9,6 +9,7 @@
 #include "proc.h"
 #include "sleeplock.h"
 
+// 初始化睡眠锁，分别初始化lk成员，包括内部的锁lk、名称name、锁状态locked和持有锁的进程pid。
 void
 initsleeplock(struct sleeplock *lk, char *name)
 {
@@ -18,38 +19,38 @@ initsleeplock(struct sleeplock *lk, char *name)
   lk->pid = 0;
 }
 
+// 获取睡眠锁。
 void
 acquiresleep(struct sleeplock *lk)
 {
-  acquire(&lk->lk);
-  while (lk->locked) {
-    sleep(lk, &lk->lk);
+  acquire(&lk->lk); // 首先获取内部的锁lk，保证操作的原子性。
+  while (lk->locked) { // 如果睡眠锁已被其他进程持有，则循环等待。
+    sleep(lk, &lk->lk); // 等待lk解锁，并在此处释放lk锁，让其他进程有机会获取它。
   }
-  lk->locked = 1;
-  lk->pid = myproc()->pid;
-  release(&lk->lk);
+  lk->locked = 1; // 当lk没有被其他进程持有时，将它标记为已锁定状态。
+  lk->pid = myproc()->pid; // 记录当前进程的PID作为持有锁的进程。
+  release(&lk->lk); // 释放内部的锁lk。
 }
 
+// 释放睡眠锁。
 void
 releasesleep(struct sleeplock *lk)
 {
-  acquire(&lk->lk);
-  lk->locked = 0;
-  lk->pid = 0;
-  wakeup(lk);
-  release(&lk->lk);
+  acquire(&lk->lk); // 首先获取内部的锁lk，保证操作的原子性。
+  lk->locked = 0; // 将睡眠锁标记为未锁定状态。
+  lk->pid = 0; // 清除持有锁的进程ID。
+  wakeup(lk); // 唤醒在lk上睡眠的进程，让它们有机会获取该锁。
+  release(&lk->lk); // 释放内部的锁lk。
 }
 
+// 检查当前进程是否持有睡眠锁。
 int
 holdingsleep(struct sleeplock *lk)
 {
   int r;
   
-  acquire(&lk->lk);
-  r = lk->locked && (lk->pid == myproc()->pid);
-  release(&lk->lk);
+  acquire(&lk->lk); // 首先获取内部的锁lk，保证操作的原子性。
+  r = lk->locked && (lk->pid == myproc()->pid); // 当lk被当前进程持有时，返回1；否则返回0。
+  release(&lk->lk); // 释放内部的锁lk。
   return r;
 }
-
-
-
