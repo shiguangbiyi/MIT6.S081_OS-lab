0084a8fe6ad728a4d710eabb0fe94a86aecd13ac
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-06 21:46:18 +0800

    完整注释fs.h

--- kernel/fs.h
+++ kernel/fs.h
@@ -1,25 +1,21 @@
-// On-disk file system format.
-// Both the kernel and user programs use this header file.
+// fs.h 文件定义了磁盘上文件系统的格式，内核和用户程序都使用这个头文件。
 
+#define ROOTINO  1   // 根目录的 i 节点编号
+#define BSIZE 1024  // 块大小
 
-#define ROOTINO  1   // root i-number
-#define BSIZE 1024  // block size
-
-// Disk layout:
-// [ boot block | super block | log | inode blocks |
-//                                          free bit map | data blocks]
+// 磁盘布局:
+// [ 引导块 | 超级块 | 日志 | i 节点块 | 空闲位图 | 数据块]
 //
-// mkfs computes the super block and builds an initial file system. The
-// super block describes the disk layout:
+// mkfs 计算超级块并构建初始文件系统。超级块描述了磁盘布局：
 struct superblock {
-  uint magic;        // Must be FSMAGIC
-  uint size;         // Size of file system image (blocks)
-  uint nblocks;      // Number of data blocks
-  uint ninodes;      // Number of inodes.
-  uint nlog;         // Number of log blocks
-  uint logstart;     // Block number of first log block
-  uint inodestart;   // Block number of first inode block
-  uint bmapstart;    // Block number of first free map block
+  uint magic;        // 必须为 FSMAGIC
+  uint size;         // 文件系统镜像的大小（块数）
+  uint nblocks;      // 数据块数
+  uint ninodes;      // i 节点数
+  uint nlog;         // 日志块数
+  uint logstart;     // 第一个日志块的块号
+  uint inodestart;   // 第一个 i 节点块的块号
+  uint bmapstart;    // 第一个空闲位图块的块号
 };
 
 #define FSMAGIC 0x10203040
@@ -28,33 +24,32 @@ struct superblock {
 #define NINDIRECT (BSIZE / sizeof(uint))
 #define MAXFILE (NDIRECT + NINDIRECT)
 
-// On-disk inode structure
+// 磁盘上的 i 节点结构
 struct dinode {
-  short type;           // File type
-  short major;          // Major device number (T_DEVICE only)
-  short minor;          // Minor device number (T_DEVICE only)
-  short nlink;          // Number of links to inode in file system
-  uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  short type;           // 文件类型
+  short major;          // 主设备号（仅对 T_DEVICE 类型有效）
+  short minor;          // 次设备号（仅对 T_DEVICE 类型有效）
+  short nlink;          // 文件系统中连接到此 i 节点的数量
+  uint size;            // 文件大小（字节数）
+  uint addrs[NDIRECT+1];   // 数据块地址
 };
 
-// Inodes per block.
+// 每块中的 i 节点数。
 #define IPB           (BSIZE / sizeof(struct dinode))
 
-// Block containing inode i
+// 包含 i 节点 i 的块
 #define IBLOCK(i, sb)     ((i) / IPB + sb.inodestart)
 
-// Bitmap bits per block
+// 每块中的位图位数
 #define BPB           (BSIZE*8)
 
-// Block of free map containing bit for block b
+// 包含块 b 的空闲位图块
 #define BBLOCK(b, sb) ((b)/BPB + sb.bmapstart)
 
-// Directory is a file containing a sequence of dirent structures.
+// 目录是一个包含一系列 dirent 结构的文件。
 #define DIRSIZ 14
 
 struct dirent {
-  ushort inum;
-  char name[DIRSIZ];
+  ushort inum; // i 节点号
+  char name[DIRSIZ]; // 目录项名称
 };
-
