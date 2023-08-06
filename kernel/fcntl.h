b12d2ca039a0af291eb3223090f7e391cb3b0241
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-06 19:14:54 +0800

    完整注释fcntl.h

--- kernel/fcntl.h
+++ kernel/fcntl.h
@@ -1,5 +1,7 @@
-#define O_RDONLY  0x000
-#define O_WRONLY  0x001
-#define O_RDWR    0x002
-#define O_CREATE  0x200
-#define O_TRUNC   0x400
+// 定义了一些文件访问标志（File Access Flags），用于在操作系统中表示文件打开时的不同模式。
+
+#define O_RDONLY  0x000    // 以只读模式打开文件
+#define O_WRONLY  0x001    // 以只写模式打开文件
+#define O_RDWR    0x002    // 以读写模式打开文件
+#define O_CREATE  0x200    // 如果文件不存在，则创建文件
+#define O_TRUNC   0x400    // 如果文件存在且以可写方式打开，则截断文件大小为0
