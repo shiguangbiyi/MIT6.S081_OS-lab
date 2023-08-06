e51f2e3d33b2abdf707b1c17c833cea42ce64a57
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-06 22:20:26 +0800

    完整注释plic.c

--- kernel/plic.c
+++ kernel/plic.c
@@ -1,3 +1,5 @@
+// plic.c 文件实现了 RISC-V 平台级中断控制器（PLIC）相关的函数。
+
 #include "types.h"
 #include "param.h"
 #include "memlayout.h"
@@ -5,30 +7,34 @@
 #include "defs.h"
 
 //
-// the riscv Platform Level Interrupt Controller (PLIC).
+// RISC-V 平台级中断控制器（PLIC）初始化函数
 //
-
 void
 plicinit(void)
 {
-  // set desired IRQ priorities non-zero (otherwise disabled).
+  // 设置所需的中断请求（IRQ）优先级为非零值（否则会被禁用）。
   *(uint32*)(PLIC + UART0_IRQ*4) = 1;
   *(uint32*)(PLIC + VIRTIO0_IRQ*4) = 1;
 }
 
+//
+// RISC-V 平台级中断控制器（PLIC）初始化函数，针对当前 Hart 初始化
+//
 void
 plicinithart(void)
 {
   int hart = cpuid();
   
-  // set uart's enable bit for this hart's S-mode. 
+  // 为当前 Hart 的 S-mode 设置 UART0 和 VIRTIO0 的使能位（enable bit）。
   *(uint32*)PLIC_SENABLE(hart)= (1 << UART0_IRQ) | (1 << VIRTIO0_IRQ);
 
-  // set this hart's S-mode priority threshold to 0.
+  // 将当前 Hart 的 S-mode 优先级阈值设置为 0。
   *(uint32*)PLIC_SPRIORITY(hart) = 0;
 }
 
-// ask the PLIC what interrupt we should serve.
+//
+// 查询 PLIC 以确定要服务的中断（IRQ）。
+//
 int
 plic_claim(void)
 {
@@ -37,7 +43,9 @@ plic_claim(void)
   return irq;
 }
 
-// tell the PLIC we've served this IRQ.
+//
+// 告知 PLIC 我们已经服务完这个中断（IRQ）。
+//
 void
 plic_complete(int irq)
 {
