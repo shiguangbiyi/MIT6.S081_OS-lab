3e105e01c93d8c3b6e6b681627c914bb814b8abb
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-08 21:36:27 +0800

    Update pgtbltest.c

--- user/pgtbltest.c
+++ user/pgtbltest.c
@@ -30,23 +30,25 @@ ugetpid_test()
 {
   int i;
 
-  printf("ugetpid_test starting\n");
-  testname = "ugetpid_test";
+  printf("ugetpid_test starting\n"); // 打印测试开始信息
+  testname = "ugetpid_test"; // 设置当前测试的名称
 
+  // 循环进行多次测试
   for (i = 0; i < 64; i++) {
-    int ret = fork();
-    if (ret != 0) {
-      wait(&ret);
+    int ret = fork(); // 创建一个子进程
+    if (ret != 0) { // 如果是父进程
+      wait(&ret); // 等待子进程结束
       if (ret != 0)
-        exit(1);
-      continue;
+        exit(1); // 子进程出错，退出
+      continue; // 继续下一个测试
     }
-    if (getpid() != ugetpid())
-      err("missmatched PID");
-    exit(0);
+    if (getpid() != ugetpid()) // 在子进程中，比较getpid()和ugetpid()
+      err("missmatched PID"); // 如果PID不匹配，报错
+    exit(0); // 子进程正常结束
   }
-  printf("ugetpid_test: OK\n");
+  printf("ugetpid_test: OK\n"); // 所有测试成功通过，打印成功信息
 }
+
 
 void
 pgaccess_test()
