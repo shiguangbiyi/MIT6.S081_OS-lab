0caedce2d55996d3fd5d0f18907f9fe62fcec7c2
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-09 00:39:42 +0800

    Update sysproc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -7,6 +7,7 @@
 #include "spinlock.h"
 #include "proc.h"
 
+
 uint64
 sys_exit(void)
 {
@@ -76,14 +77,58 @@ sys_sleep(void)
 }
 
 
-#ifdef LAB_PGTBL
+#ifdef LAB_PGTBL // 仅在定义了LAB_PGTBL时编译以下代码
+// 系统调用：报告已访问的页面
 int
 sys_pgaccess(void)
 {
-  // lab pgtbl: your code here.
-  return 0;
+  uint64 srcva, st; // 输入参数的虚拟地址和存储位掩码的用户地址
+  int len; // 要检查的页面数量
+  uint64 buf = 0; // 用于存储位掩码的缓冲区
+  struct proc *p = myproc(); // 获取当前进程
+
+  acquire(&p->lock); // 获取进程锁，确保不会发生竞争
+
+  // 解析系统调用参数
+  argaddr(0, &srcva); // 获取第一个参数：要检查的用户虚拟地址的起始位置
+  argint(1, &len);    // 获取第二个参数：要检查的页面数量
+  argaddr(2, &st);    // 获取第三个参数：存储位掩码的用户地址
+
+  // 检查页面数量是否合法,设置限制
+  if ((len > 32) || (len < 1))
+    return -1;
+
+  pte_t *pte;
+  for (int i = 0; i < len; i++) {
+    // 获取当前页面的页表项
+    pte = walk(p->pagetable, srcva + i * PGSIZE, 0);
+    if (pte == 0) {
+      release(&p->lock); // 释放进程锁
+      return -1; // 无效页面，返回错误
+    }
+    
+    // 检查页面是否有效且属于用户空间
+    if ((*pte & PTE_V) == 0 || (*pte & PTE_U) == 0) {
+      release(&p->lock); // 释放进程锁
+      return -1; // 无效页面或不属于用户空间，返回错误
+    }
+    
+    // 检查页面的访问位是否已设置
+    if (*pte & PTE_A) {
+      *pte = *pte & ~PTE_A; // 清除访问位
+      buf |= (1 << i); // 设置位掩码中的相应位
+    }
+  }
+  
+  release(&p->lock); // 释放进程锁
+
+  // 将位掩码复制到用户空间
+  copyout(p->pagetable, st, (char *)&buf, ((len - 1) / 8) + 1);
+
+  return 0; // 返回成功
 }
-#endif
+#endif // 结束LAB_PGTBL编译条件
+
 
 uint64
 sys_kill(void)
